8a3404b9e5d416e81fedb74ccd7b250046f39e3f
Ahmad Athra <ahmad.yasser.athra@gmail.com>
2025-02-08 23:37:33 +0300

    chore(jest.config.mjs): add threshold for testing coverage

--- jest.config.mjs
+++ jest.config.mjs
@@ -21,7 +21,7 @@ const config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ["src/**/*.js"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
@@ -43,7 +43,14 @@ const config = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80, // This ensures no less than 80% statement coverage
+    },
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
